d23cd46a317c16381cc5215ccc3511a6a5a2d817
mking01 <59142310+mking01@users.noreply.github.com>
2020-03-23 15:11:30 -0400

    Create ch12_exercises.py

---
+++ python_programming_3e/ch12_exercises.py
@@ -0,0 +1,38 @@
+# Ch. 12 - Object Oriented Design
+# True / False
+# 1. F
+# 2. F
+# 3. T
+# 4. T
+# 5. F
+# 6. F
+# 7. F
+# 8. F
+
+# Multiple Choice
+# 1. D
+# 2. B
+# 3. B
+# 4. C
+# 5. D
+
+# Discussion
+# 1. Object-oriented design is the process of developing a set of processes to define a problem.
+# Pre-planning and pseudo code are vital in thinking through design choices prior to implementing, although much
+# trial and error is to be expected even in light of careful planning.  Classes are often best represented by the
+# nouns in the problem you're trying to solve.  Attributes of each class can be considered characteristics of the
+# nouns in the problem being solved.  Functions within the class are actions or duties that noun would undertake -
+# look for verbs. As an example, a patient class could have attributes of patient ID, gender, and last visit date.  A
+# function could calculate time elapsed since current time (now) and most recent prior visit. Scaled, this eventually
+# represents building a flexible, dynamic program.
+
+# 2. Encapsulation - separating implementation details of an object from how the object is used
+# Polymorphism - Different classes implementing the same methods (functions),
+# allows programs to be flexible and allows code to be used in multiple different situations
+# Inheritance - creating a
+# subclass from a class which has all the class features + some addition features.  Using the patient class example
+# above, a subclass could be multiple sclerosis patients who have all the original patient attributes plus attributes
+# unique to their condition.
+
+# Exercises
+# practicing through other projects
