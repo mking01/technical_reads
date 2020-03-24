99542f9a9aaed3bbe9412b8cd3376b64fb319163
mking01 <59142310+mking01@users.noreply.github.com>
2020-03-24 10:01:28 -0400

    Create ch13_exercises.py

---
+++ python_programming_3e/ch13_exercises.py
@@ -0,0 +1,36 @@
+# Ch. 13 - Algorithm Design and Recursion
+
+# True / False
+# 1. T
+# 2. F, in and index operations
+# 3. F
+# 4. F
+# 5. F # on or more base cases
+# 6. T
+# 7. T
+# 8. F # recursive functions are generalizations of loops
+# 9. T
+# 10. T # exponential algorithms are considered intractible
+
+# Multiple Choice
+# 1. A
+# 2. C
+# 3. A
+# 4. A
+# 5. A
+# 6. D
+# 7. D
+# 8. B
+# 9. D
+# 10. B
+
+# Discussion
+# 1. Fastest to slowest algorithm run times:
+# log n, n, n log n, n**2, 2**n
+# 2. Explain two rules a proper recursive definition or function must follow:
+# 1. Must have a base case
+# 2. Must be a sequencing problem
+# 3. foo, ofo, ofo, oof
+# 4. skip
+# 5. Divide and conquer algorithms are efficient because they quickly rule out large chunks of the
+# space that needs to be searched.  Starts in the middle and splits, then repeats. (log n
